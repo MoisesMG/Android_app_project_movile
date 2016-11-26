864ae5ff2600b35a7749ecb689f17231c691f5cd
MoisesMG <menagonzalezmoises@gmail.com>
2016-11-26 16:12:08 -0600

    clase con los codigos le niveles, materias y lecciones implementada

---
+++ app/src/main/java/app/rojoyazul/com/android_app_project_movile/ContentAplication.java
@@ -0,0 +1,47 @@
+package app.rojoyazul.com.android_app_project_movile;
+
+/**
+ * Created by Moises on 26/11/2016.
+ * Ene sta clase se almacena el contenido a mostrar en las actividades
+ * Los prefijos se identifican:
+ *      L000000 para los niveles
+ *      S000000 para la materias
+ *      L000000 para las lecciones
+ *      C000000 para el contenido
+ *      V000000 para los videos
+ */
+
+public class ContentAplication {
+    /** codigos de las actividades*/
+    //NIVELES
+    public static final int [] LEVELS = {100, 200, 300, 400};
+
+    //MATERIAS
+    public static final int [] SUBJECTS_LV1 = {110, 120, 130, 140};
+    public static final int [] SUBJECTS_LV2 = {210, 220, 230, 240};
+    public static final int [] SUBJECTS_LV3 = {310, 320, 330, 340};
+    public static final int [] SUBJECTS_LV4 = {410, 420, 430, 440};
+
+    //LECCIONES
+    public static final int [] LESSONS_SUB1_LV1 = {111, 112, 113, 114};
+    public static final int [] LESSONS_SUB2_LV1 = {121, 122, 123, 124};
+    public static final int [] LESSONS_SUB3_LV1 = {131, 132, 133, 134};
+    public static final int [] LESSONS_SUB4_LV1 = {141, 142, 143, 143};
+
+    public static final int [] LESSONS_SUB1_LV2 = {211, 212, 213, 214};
+    public static final int [] LESSONS_SUB2_LV2 = {221, 222, 223, 224};
+    public static final int [] LESSONS_SUB3_LV2 = {231, 232, 233, 234};
+    public static final int [] LESSONS_SUB4_LV2 = {241, 242, 243, 243};
+
+    public static final int [] LESSONS_SUB1_LV3 = {311, 312, 313, 314};
+    public static final int [] LESSONS_SUB2_LV3 = {321, 322, 323, 324};
+    public static final int [] LESSONS_SUB3_LV3 = {331, 332, 333, 334};
+    public static final int [] LESSONS_SUB4_LV3 = {341, 342, 343, 343};
+
+    public static final int [] LESSONS_SUB1_LV4 = {411, 412, 413, 414};
+    public static final int [] LESSONS_SUB2_LV4 = {421, 422, 423, 424};
+    public static final int [] LESSONS_SUB3_LV4 = {431, 432, 433, 434};
+    public static final int [] LESSONS_SUB4_LV4 = {441, 442, 443, 443};
+
+
+}//fin de la clase
